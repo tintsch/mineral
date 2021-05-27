67d9004bf1f00d2ae805c333c3fedc93f0a19762
Valentin Keller <valentin.keller@silverlight.ch>
2021-05-27 13:57:59 +0200

    drop table tags

---
+++ database/migrations/2021_05_27_113456_drop_table_tags.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropTableTags extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	Schema::drop('tags');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -147,7 +147,7 @@ final class Versions
   'sebastian/type' => '2.3.1@81cd61ab7bbf2de744aba0ea61fae32f721df3d2',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'laravel/laravel' => 'dev-master@f461a32275ce0eb0a69de647f08e9c07b177faf6',
+  'laravel/laravel' => 'dev-master@037df7a5f6b8e2364cfa22a6145715f5a7667370',
 );
 
     private function __construct()
